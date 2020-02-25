1a4fa9a1c426bd73e134dfdeca2db419bff27cac
vladislav.sterkhov <vladislav.sterkhov@south.rt.ru>
2020-02-25 18:43:04 +0300

    refactoring

--- src/main/java/org/example/controller/MQController.java
+++ src/main/java/org/example/controller/MQController.java
@@ -1,16 +1,11 @@
 package org.example.controller;
 
-import org.example.service.MessageReceiverService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/mq/")
 public class MQController {
-
-    @Autowired
-    MessageReceiverService messageReceiverService;
 
     @GetMapping(value = "receive")
     public void findStatement() {
